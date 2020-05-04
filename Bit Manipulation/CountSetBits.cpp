9d2391995b311e175071327fde119e7d29a38bc8
Vikash Kumar <kumarv4624@gmail.com>
2020-05-04 11:47:44 +0530

    Create CountSetBits.cpp

---
+++ Bit Manipulation/CountSetBits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class gfg {
+    /* Function to get no of set bits in binary
+representation of passed binary no. */
+public:
+    unsigned int countSetBits(int n)
+    {
+        unsigned int count = 0;
+        while (n) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
+/* Program to test function countSetBits */
+int main()
+{
+    gfg g;
+    int i = 9;
+    cout << g.countSetBits(i);
+    return 0;
+}
