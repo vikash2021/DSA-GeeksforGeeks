69a7ab53e3b74a0baef738623826c1b2f4109f7c
Vikash Kumar <kumarv4624@gmail.com>
2020-05-04 11:50:53 +0530

    Update CountSetBits.cpp

--- Bit Manipulation/CountSetBits.cpp
+++ Bit Manipulation/CountSetBits.cpp
@@ -10,3 +10,8 @@
         return count;
     }
 
+//OR
+Note: In GCC, we can directly count set bits using __builtin_popcount(). So we can avoid a separate function for counting set bits.
+    
+    cout << __builtin_popcount(4) << endl;
+  //  O/P: 1
