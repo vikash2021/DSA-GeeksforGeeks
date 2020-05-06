672ad3b4dbce535105a883d6f302c95b0ec6a950
Vikash Kumar <kumarv4624@gmail.com>
2020-05-06 16:16:40 +0530

    Create MirrorOfABinaryTree.cpp

---
+++ Trees/MirrorOfABinaryTree.cpp
@@ -0,0 +1,29 @@
+void mirror(Node* root) 
+{
+    
+    if (root == NULL) 
+        return; 
+  
+    queue<Node*> q; 
+    q.push(root); 
+  
+    // Do BFS. While doing BFS, keep swapping 
+    // left and right children 
+    while (!q.empty()) 
+    { 
+        // pop top node from queue 
+        Node* curr = q.front(); 
+        q.pop(); 
+  
+        // swap left child with right child 
+        swap(curr->left, curr->right); 
+  
+        // push left and right children 
+        if (curr->left) 
+            q.push(curr->left); 
+        if (curr->right) 
+            q.push(curr->right); 
+    } 
+    
+    
+}
