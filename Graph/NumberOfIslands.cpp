662eba39ece0fcd5ceb54c25bb32ab484e41335b
Vikash Kumar <kumarv4624@gmail.com>
2020-05-07 14:22:22 +0530

    Update NumberOfIslands.cpp

--- Graph/NumberOfIslands.cpp
+++ Graph/NumberOfIslands.cpp
@@ -21,49 +21,32 @@ int main() {
         cout << findIslands(A, N, M) << endl;
     }
     return 0;
-}// } Driver Code Ends
 
-
-/*you are required to complete this method*/
-
-/*  Function to find number of islands in the given graph
-*   A[]: input array
-*   N, M: Row and column of given matrix
-*/
-int isSafe(vector<int> M[], int row, int col, vector<vector<bool>> &visited,
-           int ROW, int COL) {
-    // Check boundary conditions for i and j. Also check if node is not already
-    // visited and is currently 1.
-    return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL) &&
-           (M[row][col] && !visited[row][col]);
+int isSafe(vector<int> M[], int row, int col, vector<vector<bool>> &visited,int ROW, int COL)
+{
+   
+    return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL) && (M[row][col] && !visited[row][col]);
 }
 
-void dfs(vector<int> A[], int i, int j, vector<vector<bool>> &vis, int N,
-         int M) {
+void dfs(vector<int> A[], int i, int j, vector<vector<bool>> &vis, int N,int M) 
+{
 
-    int row[] = {-1, -1, -1, 0,
-                 0,  1,  1,  1}; // Check for all 8 neighbours of i and j
+    int row[] = {-1, -1, -1, 0, 0,  1,  1,  1}; // Check for all 8 neighbours of i and j
     int col[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
     vis[i][j] = true;
 
     for (int k = 0; k < 8; k++) // Checking for all 8 neighbouring sides
     {
-        if ((i + row[k] < N && j + col[k] < M) &&
-            (isSafe(A, i + row[k], j + col[k], vis, N,
-                    M))) { // checking if node is not visited
-            dfs(A, i + row[k], j + col[k], vis, N,
-                M); // and A[i+row[k]][j+col[k]] is 1, call dfs again
+        if ((i + row[k] < N && j + col[k] < M) &&(isSafe(A, i + row[k], j + col[k], vis, N,M))) { // checking if node is not visited
+            dfs(A, i + row[k], j + col[k], vis, N, M); // and A[i+row[k]][j+col[k]] is 1, call dfs again
                     // exit(0);
         }
     }
 }
 
 int findIslands(vector<int> A[], int N, int M) {
-    vector<vector<bool>> visited(
-        N,
-        vector<bool>(
-            M, false)); // Created a visited array intially all false(unvisited)
+    vector<vector<bool>> visited(N,vector<bool>(M, false)); // Created a visited array intially all false(unvisited)
     int count = 0;      // Number of different islands
     for (int i = 0; i < N; i++) // Loop through every point in the matrix
     {
