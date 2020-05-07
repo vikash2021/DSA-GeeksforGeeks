a2638eae42dfa7c9f4ce7d88bc366e596afb1d6e
Vikash Kumar <kumarv4624@gmail.com>
2020-05-07 18:09:24 +0530

    Create XTotalShapes.cpp

---
+++ Graph/XTotalShapes.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<string> &A, int x, int y){
+    if(x>=A.size()||A[x].length()<=y){
+        return ;
+    }
+	if(A[x][y]=='X'){
+		A[x][y]='O';
+		dfs(A,x+1,y);dfs(A,x,y+1);dfs(A,x-1,y);dfs(A,x,y-1);
+	}
+}
+
+int fun(vector<string> &A) {
+    int i,j,ans=0;
+	for(i=0;i<A.size();i++){
+		for(j=0;j<A[i].length();j++){
+			if(A[i][j]=='X'){
+				ans++;
+				dfs(A, i,j);
+			}
+		}
+	}
+	return ans;
+}
+
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int m;
+		cin>>m;
+		vector<string> A;
+		for(int i=0;i<n;i++){
+			string S;
+			cin>>S;
+			A.push_back(S);
+		}
+		cout<<fun(A)<<"\n";
+	}
+}
